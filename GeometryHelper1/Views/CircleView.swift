da3e05c5b3e3c5759f6d3a5571f014360e0a1fe4
Benjamin Lavallee <blavallee@lcs.on.ca>
2024-01-15 11:15:43 -0500

    Organized project by adding Views and Model groups and a new view file named CircleView.

---
+++ GeometryHelper1/Views/CircleView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleView.swift
+//  GeometryHelper1
+//
+//  Created by Benjamin Lavallee on 2024-01-15.
+//
+
+import SwiftUI
+
+struct CircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleView()
+}
