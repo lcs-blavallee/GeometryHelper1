9d0a1cb5e96bad53a41d242ca5a90c4d38d832da
Benjamin Lavallee <blavallee@lcs.on.ca>
2024-01-15 11:10:39 -0500

    Initial Commit

---
+++ GeometryHelper1/GeometryHelper1App.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryHelper1App.swift
+//  GeometryHelper1
+//
+//  Created by Benjamin Lavallee on 2024-01-15.
+//
+
+import SwiftUI
+
+@main
+struct GeometryHelper1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
